1e9d57ef8debc7bae345cc91dbea6af0fc056235
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2016-02-24 01:45:36 -0800

    Added a Export to XML function to the Subjects list
    - Added flyouts for notification
    - Added a file picker to set location of file

--- UberEversolWin10/UberEversolWin10/Pages/SubjectList.xaml.cs
+++ UberEversolWin10/UberEversolWin10/Pages/SubjectList.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using UberEversol.DataModel;
+using System.Xml;
+using System.Text;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -162,6 +164,90 @@ namespace UberEversol.Pages
         private void subject_list_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             AppBtnEdit.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Saves the subject list to an xml file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void AppBtnExportSubjects_Click(object sender, RoutedEventArgs e)
+        {
+            XmlWriter writer;
+            StringWriter sw;
+
+            using (sw = new StringWriter())
+            {
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                settings.NewLineOnAttributes = true;
+
+                //using (writer = XmlWriter.Create(new StringBuilder("subjects.xml")))
+                using (writer = XmlWriter.Create(sw, settings))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Employees");
+
+                    foreach (Subject person in subjectList)
+                    {
+                        writer.WriteStartElement("Subject");
+
+                        writer.WriteElementString("id", person.id.ToString());
+                        writer.WriteElementString("first_name", person.first_name);
+                        writer.WriteElementString("last_name", person.last_name);
+                        writer.WriteElementString("created", person.created.ToString());
+                        writer.WriteElementString("hit_count", person.hit_count.ToString());
+                        writer.WriteElementString("recording_count", person.recording_count.ToString());
+                        writer.WriteElementString("user_rating", person.user_rating.ToString());
+                        writer.WriteElementString("image", Convert.ToBase64String(person.image));
+                        //byte[] decByte3 = Convert.FromBase64String(s3); to undo
+                        writer.WriteElementString("active", person.active.ToString());
+
+                        writer.WriteEndElement();
+                    }
+
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+            }
+
+            var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("XML", new List<string>() { ".xml" });
+
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "subjects";
+
+            //Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until
+                // we finish making changes and call CompleteUpdatesAsync.
+                Windows.Storage.CachedFileManager.DeferUpdates(file);
+
+                // write to file
+                await Windows.Storage.FileIO.WriteTextAsync(file, sw.ToString());
+
+                // Let Windows know that we're finished changing the file so
+                // the other app can update the remote version of the file.
+                // Completing updates may require Windows to ask for user input.
+                Windows.Storage.Provider.FileUpdateStatus status = await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+
+                if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                {
+                    FlyoutBase.SetAttachedFlyout(this, (FlyoutBase)this.Resources["notifyFlyout_saved"]);
+                    FlyoutBase.ShowAttachedFlyout(this);
+                }
+                else
+                {
+                    FlyoutBase.SetAttachedFlyout(this, (FlyoutBase)this.Resources["notifyFlyout_error"]);
+                    FlyoutBase.ShowAttachedFlyout(this);
+                }
+            }
         }
     }
 }
