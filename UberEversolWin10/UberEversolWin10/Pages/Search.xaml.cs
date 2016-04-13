c888f792ae9e3909f5f9a269a9d298a4eca4e8b9
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2016-04-12 23:09:19 -0700

    Updated Search Page
    - Added Search field with button and search type
    - Added list views and pulled data from database to populate views
    - List views still need to be visually formatted

--- UberEversolWin10/UberEversolWin10/Pages/Search.xaml.cs
+++ UberEversolWin10/UberEversolWin10/Pages/Search.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using UberEversol.DataModel;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +26,54 @@ namespace UberEversol.Pages
         public Search()
         {
             this.InitializeComponent();
+        }
+
+        private async void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            String qryStr = txtQry.Text;
+            int qryType = cboFields.SelectedIndex;
+
+            lstvResultsSessions.Visibility = Visibility.Collapsed;
+            lstvResultsTracks.Visibility = Visibility.Collapsed;
+
+            if (!String.IsNullOrEmpty(qryStr))  // Check input value
+            {
+                using (var db = new UberEversolContext())
+                {
+                    List<Session> ses_results;
+                    List<Track> trk_results;
+
+                    if (qryType == 0)
+                    {
+                        ses_results = db.Sessions.Where(s => s.title.Contains(qryStr)
+                                || s.description.Contains(qryStr)).ToList();
+
+                        txtSesResultCount.Text = ses_results.Count + " record(s) found!";
+                        lstvResultsSessions.ItemsSource = ses_results;
+                        lstvResultsSessions.Visibility = Visibility.Visible;
+                    }
+                    else if (qryType == 1)
+                    {
+                        trk_results = db.Tracks.Where(t => t.title.Contains(qryStr) 
+                                || t.description.Contains(qryStr) 
+                                || t.subject.first_name.Contains(qryStr)
+                                || t.subject.last_name.Contains(qryStr)).ToList();
+
+                        
+                        foreach (Track t in trk_results)
+                        {
+                            t.loadStructures();  // Populates the Icollection objects of track
+
+                            if (t.subject != null)
+                                t.subject.imageObj = await t.subject.loadImage();
+                        }
+
+                        txtTrkResultCount.Text = trk_results.Count + " record(s) found!";
+                        lstvResultsTracks.ItemsSource = trk_results;
+                        lstvResultsTracks.Visibility = Visibility.Visible;
+                    }
+                }
+            }
         }
     }
 }
